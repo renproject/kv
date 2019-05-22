852b8086dda97ba78247a1eb4dd295272b7eeb30
Loong <loong@republicprotocol.com>
2019-05-22 11:46:19 +0800

    kv: re-export only high-level implementations

--- kv.go
+++ kv.go
@@ -7,24 +7,38 @@ package kv
 import (
 	"github.com/renproject/kv/badgerdb"
 	"github.com/renproject/kv/db"
+	"github.com/renproject/kv/gob"
 	"github.com/renproject/kv/json"
 	"github.com/renproject/kv/leveldb"
 	"github.com/renproject/kv/memdb"
+	"github.com/renproject/kv/store"
 )
 
 var (
 	// ErrNotFound is returned when there is no value associated with a key.
-	ErrNotFound = db.ErrKeyNotFound
+	ErrNotFound = db.ErrNotFound
 )
 
 var (
-	DB = db.DB
+	// TODO: Comment!
+	Store = store.Store
 
-	IterableDB = db.IterableDB
+	// TODO: Comment!
+	Iterable = store.Iterable
 
-	// Iterator defines a standard interface for iterating over key-value pairs
-	// from an IterableStore.
-	Iterator = db.Iterator
+	// TODO: Comment!
+	Iterator = store.Iterator
+)
+
+var (
+	// NewJSON returns a key-value store that marshals and unmarshals values
+	// using the standard Golang JSON marshalers. For more information, see
+	// https://golang.org/pkg/encoding/json.
+	NewJSON = json.NewStore
+
+	// NewGob returns a key-value store that encodes and decodes values using
+	// gob. For more information, see https://golang.org/pkg/encoding/gob.
+	NewGob = gob.NewStore
 )
 
 var (
@@ -41,11 +55,4 @@ var (
 	// implementation is fast, but should not be used for persistent data
 	// storage, and does not support iteration.
 	NewMemDB = memdb.New
-)
-
-var (
-	// NewJSON returns a key-value store that marshals and unmarshals keys using
-	// the standard Golang JSON marshalers. For more information, see
-	// https://golang.org/pkg/encoding/json.
-	NewJSON = json.NewStore
 )
