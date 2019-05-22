0fb0cd1656615ed5e4655b5368fef9f54ed54bc5
Loong <loong@republicprotocol.com>
2019-05-22 11:45:13 +0800

    leveldb: fix compilation issues

--- leveldb/leveldb.go
+++ leveldb/leveldb.go
@@ -1,8 +1,7 @@
 package leveldb
 
 import (
-	"encoding/json"
-
+	"github.com/renproject/kv/db"
 	"github.com/syndtr/goleveldb/leveldb"
 )
 
@@ -10,50 +9,24 @@ type ldb struct {
 	db *leveldb.DB
 }
 
-// NewLevelDB returns a levelDB implementation of the Store.
-func NewLevelDB(db *leveldb.DB) Store {
+func New(db *leveldb.DB) db.DB {
 	return &ldb{
 		db: db,
 	}
 }
 
-// Read implements the `Store` interface.
-func (db *ldb) Read(key string, value interface{}) error {
-	data, err := db.db.Get([]byte(key), nil)
-	if err != nil {
-		if err == leveldb.ErrNotFound {
-			err = ErrKeyNotFound
-		}
-		return err
-	}
-	return json.Unmarshal(data, value)
+func (ldb *ldb) Insert(key string, data []byte) error {
+	return ldb.db.Put([]byte(key), data, nil)
 }
 
-// ReadData implements the `Store` interface.
-func (db *ldb) ReadData(key string) ([]byte, error) {
-	value, err := db.db.Get([]byte(key), nil)
+func (ldb *ldb) Get(key string) (value []byte, err error) {
+	value, err = ldb.db.Get([]byte(key), nil)
 	if err == leveldb.ErrNotFound {
-		err = ErrKeyNotFound
+		err = db.ErrNotFound
 	}
-
-	return value, err
+	return
 }
 
-// Write implements the `Store` interface.
-func (db *ldb) Write(key string, value interface{}) error {
-	data, err := json.Marshal(value)
-	if err != nil {
-		return err
-	}
-	return db.db.Put([]byte(key), data, nil)
-}
-
-// WriteData implements the `Store` interface.
-func (db *ldb) WriteData(key string, data []byte) error {
-	return db.db.Put([]byte(key), data, nil)
-}
-
-// Delete implements the `Store` interface.
-func (db *ldb) Delete(key string) error {
-	return db.db.Delete([]byte(key), nil)
+func (ldb *ldb) Delete(key string) error {
+	return ldb.db.Delete([]byte(key), nil)
 }
