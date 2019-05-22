7cbb27ee9d6a1777699afdfda0b34b3f36d39be2
Loong <loong@republicprotocol.com>
2019-05-22 11:46:03 +0800

    store: define store interfaces over db interfaces

---
+++ store/store.go
@@ -0,0 +1,18 @@
+package store
+
+type Store interface {
+	Insert(key string, value interface{}) error
+	Get(key string, value interface{}) error
+	Delete(key string) error
+}
+
+type Iterable interface {
+	Size() (int, error)
+	Iterator() (Iterator, error)
+}
+
+type Iterator interface {
+	Next() bool
+	Key() (string, error)
+	Value(value interface{}) error
+}
