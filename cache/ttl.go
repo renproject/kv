d12d1dbef9f7f40cfbb590e6995a63103fae4757
tok-kkk <yunshi@republicprotocol.com>
2019-08-01 09:55:25 +1000

    clean up old unused coded

--- cache/ttl.go
+++ cache/ttl.go
@@ -1,140 +1,140 @@
 package cache
-
-import (
-	"sync"
-	"time"
-
-	"github.com/renproject/kv/db"
-	"github.com/renproject/kv/store"
-)
-
-type ttl struct {
-	iterable   store.Iterable
-	timeToLive time.Duration
-
-	lastSeenMu *sync.Mutex
-	lastSeen   map[string]time.Time
-}
-
-// NewTTL returns a cache that wraps an underlying store. Keys that have no been
-// accessed for the specified duration will be automatically deleted from the
-// underlying store. It is safe for concurrent use, as long as the underlying
-// store is also safe for concurrent use.
-func NewTTL(iterable store.Iterable, timeToLive time.Duration) (store.Iterable, error) {
-	lastSeen := map[string]time.Time{}
-	now := time.Now()
-	iter, err := iterable.Iterator()
-	if err != nil {
-		return nil, err
-	}
-	for iter.Next() {
-		key, err := iter.Key()
-		if err != nil {
-			return nil, err
-		}
-		lastSeen[key] = now
-	}
-
-	return &ttl{
-		iterable:   iterable,
-		timeToLive: timeToLive,
-
-		lastSeenMu: new(sync.Mutex),
-		lastSeen:   lastSeen,
-	}, nil
-}
-
-// Insert a value into the underlying store. The key will have its access time
-// set to the current time.
-func (cache *ttl) Insert(key string, value interface{}) error {
-	if err := cache.iterable.Insert(key, value); err != nil {
-		return err
-	}
-
-	cache.lastSeenMu.Lock()
-	defer cache.lastSeenMu.Unlock()
-	cache.lastSeen[key] = time.Now()
-
-	return nil
-}
-
-// Get a value from the underlying store. The key will have its access time
-// updated.
-func (cache *ttl) Get(key string, value interface{}) error {
-	cache.lastSeenMu.Lock()
-	defer cache.lastSeenMu.Unlock()
-
-	lastSeen, ok := cache.lastSeen[key]
-	if !ok {
-		return db.ErrKeyNotFound
-	}
-	if time.Now().After(lastSeen.Add(cache.timeToLive)) {
-		if err := cache.deleteWithoutLock(key); err != nil {
-			return err
-		}
-		return ErrExpired
-	}
-	cache.lastSeen[key] = time.Now()
-
-	return cache.iterable.Get(key, value)
-}
-
-// Delete a value from the underlying store.
-func (cache *ttl) Delete(key string) error {
-	cache.lastSeenMu.Lock()
-	defer cache.lastSeenMu.Unlock()
-
-	return cache.deleteWithoutLock(key)
-}
-
-// Size returns the size of the underlying store.
-func (cache *ttl) Size() (int, error) {
-	cache.lastSeenMu.Lock()
-	defer cache.lastSeenMu.Unlock()
-
-	counter := 0
-	now := time.Now()
-	for key, lastSeen := range cache.lastSeen {
-		if now.Sub(lastSeen) > cache.timeToLive {
-			if err := cache.deleteWithoutLock(key); err != nil {
-				return 0, err
-			}
-		} else {
-			counter++
-		}
-	}
-
-	return counter, nil
-}
-
-// Iterator returns an iterator that can iterate over all key-value tuples in
-// the underlying store. All keys in the cache will have their access times
-// updated.
-func (cache *ttl) Iterator() (store.Iterator, error) {
-	cache.lastSeenMu.Lock()
-	defer cache.lastSeenMu.Unlock()
-
-	now := time.Now()
-	for key, lastSeen := range cache.lastSeen {
-		if now.Sub(lastSeen) > cache.timeToLive {
-			if err := cache.deleteWithoutLock(key); err != nil {
-				return nil, err
-			}
-		} else {
-			cache.lastSeen[key] = now
-		}
-	}
-
-	return cache.iterable.Iterator()
-}
-
-// The `deleteWithoutLock` method will delete a key-value tuple without locking
-// the `lastSeenMu` mutex. This method must only be called from methods that
-// have already acquired a lock on the `lastSeenMu` method.
-func (cache *ttl) deleteWithoutLock(key string) error {
-	if err := cache.iterable.Delete(key); err != nil {
-		return err
-	}
-	delete(cache.lastSeen, key)
-	return nil
-}
+//
+// import (
+// 	"sync"
+// 	"time"
+//
+// 	"github.com/renproject/kv/db"
+// 	"github.com/renproject/kv/store"
+// )
+//
+// type ttl struct {
+// 	iterable   store.Iterable
+// 	timeToLive time.Duration
+//
+// 	lastSeenMu *sync.Mutex
+// 	lastSeen   map[string]time.Time
+// }
+//
+// // NewTTL returns a cache that wraps an underlying store. Keys that have no been
+// // accessed for the specified duration will be automatically deleted from the
+// // underlying store. It is safe for concurrent use, as long as the underlying
+// // store is also safe for concurrent use.
+// func NewTTL(iterable store.Iterable, timeToLive time.Duration) (store.Iterable, error) {
+// 	lastSeen := map[string]time.Time{}
+// 	now := time.Now()
+// 	iter, err := iterable.Iterator()
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	for iter.Next() {
+// 		key, err := iter.Key()
+// 		if err != nil {
+// 			return nil, err
+// 		}
+// 		lastSeen[key] = now
+// 	}
+//
+// 	return &ttl{
+// 		iterable:   iterable,
+// 		timeToLive: timeToLive,
+//
+// 		lastSeenMu: new(sync.Mutex),
+// 		lastSeen:   lastSeen,
+// 	}, nil
+// }
+//
+// // Insert a value into the underlying store. The key will have its access time
+// // set to the current time.
+// func (cache *ttl) Insert(key string, value interface{}) error {
+// 	if err := cache.iterable.Insert(key, value); err != nil {
+// 		return err
+// 	}
+//
+// 	cache.lastSeenMu.Lock()
+// 	defer cache.lastSeenMu.Unlock()
+// 	cache.lastSeen[key] = time.Now()
+//
+// 	return nil
+// }
+//
+// // Get a value from the underlying store. The key will have its access time
+// // updated.
+// func (cache *ttl) Get(key string, value interface{}) error {
+// 	cache.lastSeenMu.Lock()
+// 	defer cache.lastSeenMu.Unlock()
+//
+// 	lastSeen, ok := cache.lastSeen[key]
+// 	if !ok {
+// 		return db.ErrKeyNotFound
+// 	}
+// 	if time.Now().After(lastSeen.Add(cache.timeToLive)) {
+// 		if err := cache.deleteWithoutLock(key); err != nil {
+// 			return err
+// 		}
+// 		return ErrExpired
+// 	}
+// 	cache.lastSeen[key] = time.Now()
+//
+// 	return cache.iterable.Get(key, value)
+// }
+//
+// // Delete a value from the underlying store.
+// func (cache *ttl) Delete(key string) error {
+// 	cache.lastSeenMu.Lock()
+// 	defer cache.lastSeenMu.Unlock()
+//
+// 	return cache.deleteWithoutLock(key)
+// }
+//
+// // Size returns the size of the underlying store.
+// func (cache *ttl) Size() (int, error) {
+// 	cache.lastSeenMu.Lock()
+// 	defer cache.lastSeenMu.Unlock()
+//
+// 	counter := 0
+// 	now := time.Now()
+// 	for key, lastSeen := range cache.lastSeen {
+// 		if now.Sub(lastSeen) > cache.timeToLive {
+// 			if err := cache.deleteWithoutLock(key); err != nil {
+// 				return 0, err
+// 			}
+// 		} else {
+// 			counter++
+// 		}
+// 	}
+//
+// 	return counter, nil
+// }
+//
+// // Iterator returns an iterator that can iterate over all key-value tuples in
+// // the underlying store. All keys in the cache will have their access times
+// // updated.
+// func (cache *ttl) Iterator() (store.Iterator, error) {
+// 	cache.lastSeenMu.Lock()
+// 	defer cache.lastSeenMu.Unlock()
+//
+// 	now := time.Now()
+// 	for key, lastSeen := range cache.lastSeen {
+// 		if now.Sub(lastSeen) > cache.timeToLive {
+// 			if err := cache.deleteWithoutLock(key); err != nil {
+// 				return nil, err
+// 			}
+// 		} else {
+// 			cache.lastSeen[key] = now
+// 		}
+// 	}
+//
+// 	return cache.iterable.Iterator()
+// }
+//
+// // The `deleteWithoutLock` method will delete a key-value tuple without locking
+// // the `lastSeenMu` mutex. This method must only be called from methods that
+// // have already acquired a lock on the `lastSeenMu` method.
+// func (cache *ttl) deleteWithoutLock(key string) error {
+// 	if err := cache.iterable.Delete(key); err != nil {
+// 		return err
+// 	}
+// 	delete(cache.lastSeen, key)
+// 	return nil
+// }
--- kv.go
+++ kv.go
@@ -4,54 +4,50 @@
 package kv
 
 import (
-	"github.com/renproject/kv/badgerdb"
-	"github.com/renproject/kv/cache"
-	"github.com/renproject/kv/db"
-	"github.com/renproject/kv/gob"
-	"github.com/renproject/kv/json"
-	"github.com/renproject/kv/memdb"
-	"github.com/renproject/kv/store"
-)
+	"errors"
 
+	"github.com/renproject/kv/badgerdb"
+	"github.com/renproject/kv/codec"
+	"github.com/renproject/kv/db"
+	"github.com/renproject/kv/memdb"
+)
 var (
 	// ErrKeyNotFound is returned when there is no value associated with a key.
-	ErrNotFound = db.ErrKeyNotFound
+	ErrKeyNotFound = errors.New("key not found")
+
+	// ErrEmptyKey is returned when key is empty.
+	ErrEmptyKey = errors.New("key cannot be empty")
+
+	// ErrIndexOutOfRange is returned when the iterator index is not in a valid range.
+	ErrIndexOutOfRange = errors.New("iterator index out of range")
+
+	// ErrTableAlreadyExists is returned when the table with given name is already exists in the db.
+	ErrTableAlreadyExists = errors.New("table already exists")
+
+	// ErrTableNotFound is returned when there is no table with given name.
+	ErrTableNotFound = errors.New("table not found")
 )
 
 type (
 	// TODO: Comment!
-	Store = store.Store
+	Table = db.Table
 
 	// TODO: Comment!
-	Iterable = store.Iterable
+	DB = db.DB
 
 	// TODO: Comment!
-	Iterator = store.Iterator
+	Codec = db.Codec
+
+	// TODO: Comment!
+	Iterator = db.Codec
 )
 
+// In-memory implementation of the DB and table
 var (
-	// NewJSON returns a key-value store that marshals and unmarshals values
-	// using the standard Golang JSON marshalers. For more information, see
-	// https://golang.org/pkg/encoding/json.
-	NewJSON = json.New
 
-	// NewGob returns a key-value store that encodes and decodes values using
-	// gob. For more information, see https://golang.org/pkg/encoding/gob.
-	NewGob = gob.New
-)
+	// TODO: Comment!
+	NewMemTable = memdb.NewTable
 
-var (
-	// NewTTLCache returns a cache that wraps an underlying store. Keys that have
-	// no been accessed for the specified duration will be automatically deleted
-	// from the underlying store. It is safe for concurrent use, as long as the
-	// underlying store is also safe for concurrent use.
-	NewTTLCache = cache.NewTTL
-)
-
-var (
-	// NewBadgerDB returns a key-value database that is implemented using
-	// BadgerDB. For more information, see https://github.com/dgraph-io/badger.
-	NewBadgerDB = badgerdb.New
 
 	// NewMemDB returns a key-value database that is implemented in-memory. This
 	// implementation is fast, but does not store data on-disk. A time-to-live can
@@ -61,3 +57,33 @@ var (
 	// use.
 	NewMemDB = memdb.New
 )
+
+// BadgerDB implementation of the DB and table
+var (
+	// TODO: Comment!
+	NewBadgerTable = badgerdb.NewTable
+
+	// NewBadgerDB returns a key-value database that is implemented using
+	// BadgerDB. For more information, see https://github.com/dgraph-io/badger.
+	NewBadgerDB = badgerdb.New
+)
+
+var (
+	// JsonCodec is a json codec that marshals and unmarshals values using the
+	// standard Golang JSON marshalers. For more information, see
+	// https://golang.org/pkg/encoding/json.
+	JsonCodec = codec.JsonCodec
+
+	// GobCodec is a gob codec that encodes and decodes values using gob. For
+	// more information, see https://golang.org/pkg/encoding/gob.
+	GobCodec = codec.GobCodec
+)
+
+// var (
+// 	// NewTTLCache returns a cache that wraps an underlying store. Keys that have
+// 	// no been accessed for the specified duration will be automatically deleted
+// 	// from the underlying store. It is safe for concurrent use, as long as the
+// 	// underlying store is also safe for concurrent use.
+// 	NewTTLCache = cache.NewTTL
+// )
+
